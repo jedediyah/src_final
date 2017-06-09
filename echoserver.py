c89cbc360759172af21dd5aa51ae5c57a5a02e4f
Jedediyah <jedediyah@gmail.com>
2017-06-09 15:47:53 -0400

    Create echoserver.py

--- echoserver.py
+++ echoserver.py
@@ -4,7 +4,7 @@ import sys
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
-server_address = ('localhost', 5005)
+server_address = ('localhost', 6006)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
