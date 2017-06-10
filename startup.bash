f2f257a83f97fa256ff402f949bc95160c53734a
Jedediyah <jedediyah@gmail.com>
2017-06-10 15:21:20 -0400

    Create startup.bash

--- startup.bash
+++ startup.bash
@@ -2,7 +2,7 @@
 python -m SimpleHTTPServer 8000 &
 python echoserver.py & 
 
-python wfield_receiver.py &
-python whalers_field_control.py &
+#python wfield_receiver.py &
+#python whalers_field_control.py &
 
 roscore -p 8001
