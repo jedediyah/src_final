fe94c8eb20409078a9d10a71b37e9542b76daa9e
jed <jedediyah@gmail.com>
2017-06-10 15:57:17 -0400

    Comms test

--- echoserver.py
+++ echoserver.py
@@ -1,37 +1,24 @@
+#!/usr/bin/env python
+
+# Test file for vpn communications
+
 import socket
-import sys
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Bind the socket to the port
-server_address = ('localhost', 6006)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
+#REC_IP = "127.0.0.1"
+REC_IP = '192.168.2.10'
+REC_PORT = 5005
 
-# Listen for incoming connections
-sock.listen(1)
+#SEND_IP = "127.0.0.1"
+SEND_IP = '192.168.2.150'
+SEND_PORT = 6006
+
+sock_rec = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock_rec.bind((REC_IP, REC_PORT))
 
 while True:
-    # Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
-    
-    try:
-        print >>sys.stderr, 'connection from', client_address
-
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            print >>sys.stderr, 'received "%s"' % data
-            if data:
-                print >>sys.stderr, 'sending data back to the client'
-                connection.sendall(data)
-            else:
-                print >>sys.stderr, 'no more data from', client_address
-                break
-            
-    finally:
-        # Clean up the connection
-        connection.close()
-    
-    
+    data, addr = sock_rec.recvfrom(1024) # buffer size is 1024 bytes
+    print "received message:", data
+    sock_send = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+    sock_send.sendto(data, (SEND_IP, SEND_PORT))
