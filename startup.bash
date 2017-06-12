6a17021dd35e064d4a925533a74b92ffcaae7bbd
jed <jedediyah@gmail.com>
2017-06-11 23:22:16 -0400

    update

--- startup.bash
+++ startup.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+roscore -p 8001 &
+sleep 5
+
 python -m SimpleHTTPServer 8000 &
-python echoserver.py & 
+#python echoserver.py & 
 
-#python wfield_receiver.py &
-#python whalers_field_control.py &
+python wfield_receiver.py &
+python whalers_field_control.py &
 
-roscore -p 8001
+
